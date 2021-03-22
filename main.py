44a6da581f4aeb8bfb58aa0fed262e787104baf6
MrBrazil <lucasfrias878@gmail.com>
2021-03-22 14:49:48 +0000

    0.5.6 Major GUI change for future plans, easier text.

--- main.py
+++ main.py
@@ -3,16 +3,21 @@ import tkinter as tk
 import tkinter.messagebox as msgbox
 import parser
 import requests
-print (parser.get_title("<p>lol</p><title>oof</title>"))
 #Warning, this is some pretty messy code right now
 #Preset webtext to the home page
-webtext = "Welcome to iMuffin Visual Alpha! You are running version 0.5.5\nNewest Features\n___________________________________________________\nBrand new GUI interface!\nEnd of Page!\nBetter documentation.\nMore responsive interface!\nTitles"
-#This is the web library of 0.4.2, a little modified to incorperate some built in functions
+webtext = "Welcome to iMuffin Visual Alpha! You are running version 0.5.6\nNewest Features\n___________________________________________________\nwww.google.com now defaults to https\nAdress bar now larger to show how enter works.\nWebsite text more or less centered with buttons on left side and search bar and website text on right side.\n GUI changes" 
+#This is the web library of 0.4.2, somewhat modified.
 #This function gets the website and returns it in text
 def get_website():
-  #try:
+  try:
     #Get the users' query.
-    body = requests.get(search.get(1.0, "end-1c"))
+    #Automatically https if the user does not specify.
+    if "https://" in str(search.get(1.0, "end-1c")):
+      #print ("got here")
+      body = requests.get(search.get(1.0, "end-1c"))
+    if "https://" not in str(search.get(1.0, "end-1c")):
+      if "http://" not in str(search.get(1.0, "end-1c")):
+       body = requests.get("https://" + search.get(1.0, "end-1c"))      
     #This places the parsed html in the website text, plus an end of page, and list of links belowards.
     window.title(parser.get_title(body) + " - iMuffin")
     website_text = parser.the_parse(body.text) + "End Page\n__________________________________\nList of links\n__________________________________\n" + parser.link_list(body.text)
@@ -22,32 +27,32 @@ def get_website():
     webtext = website_text
     website_body_text.insert(tk.END, webtext)
   #Error handling
-  #except:
-    #window.title("Error - iMuffin")
+  except:
+    window.title("Error - iMuffin")
     #If the url could be found, but an error still occured
-    #try:
-      #requests.get(search.get(1.0, "end-1c"))
-      #msgbox.showerror("Uh Oh....",  "A software error occured.") 
+    try:
+      requests.get(search.get(1.0, "end-1c"))
+      msgbox.showerror("Uh Oh",  "A software error occured.") 
     #If the URL is invalid.
-    #except:
-      #msgbox.showerror("Aw, Snap!",  "Invalid URL. Does this website exist?") 
+    except:
+      msgbox.showerror("Snap!",  "Invalid URL. Does this website exist?") 
 
 #Starting out dimension
 window = Tk()
 window.resizable(False, False)
 window.title("Home - iMuffin")
-window.geometry('700x450')
+window.geometry('700x400')
 
 #Submit Button
 gobutton = Button(window,
-	text = "Let's Go!",
+	text = "Open URL",
 	command = get_website,
   width = 6,
   height = 1,
 )  
 #Search Button
 search = tk.Text(window, 
-                   height = 1, 
+                   height = 2, 
                    width = 45) 
 
 #Website Text
@@ -57,8 +62,8 @@ website_body_text = tk.Text(
   width=70, 
   )
 #Pack all parts of the application
-gobutton.grid(column=0, row=0)  
-search.grid(column=1, row=0)
-website_body_text.grid(column=1, row=5)
+gobutton.grid(column=1, row=0)  
+search.grid(column=0, row=0)
+website_body_text.grid(column=0, row=7)
 website_body_text.insert(tk.END, webtext)
 window.mainloop()
